cf9b7c48964945454302ccf986f1a6a86b27af08
Cheng Chang <cheng.chang@team.telstra.com>
2018-07-20 16:56:07 +1000

    use mongo db model

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var todo = require('./todo');
+var task = require('./task');
 
 router.get('/', function(req, res, next) {
   res.send('home');
 })
 
-router.use('/todo', todo);
+router.use('/todo', task);
 
 module.exports = router;
